02dfdef0a6fe2f188f7a57adb4507027b31a5074
maycon-alves <maycon_valdemiro@hotmail.com>
2023-06-08 11:18:50 -0300

    motor de analise

--- src/main/java/br/com/projetoFintech/controller/CadastroServelet.java
+++ src/main/java/br/com/projetoFintech/controller/CadastroServelet.java
@@ -20,51 +20,101 @@ import br.com.projetoFintech.model.Usuario;
 @WebServlet("/cadastro")
 public class CadastroServelet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public CadastroServelet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#HttpServlet()
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		System.out.println("aqui estamos1");
-		UsuarioDAO user = new UsuarioDAO();
-		String cpf = request.getParameter("cpf");
-		System.out.println(request.getParameter("cpf"));
-		response.getWriter().append("sucesso");
+	public CadastroServelet() {
+		super();
+		// TODO Auto-generated constructor stub
 	}
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		System.out.println("==========GET-INICIO=============");
+		request.getRequestDispatcher("cadastro.jsp").forward(request, response);
+		System.out.println("==========GET-FIM=============");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		System.out.println("==========POST=============");
-		UsuarioDAO dao = new UsuarioDAO();
 		String cpf = request.getParameter("cpf");
 		String nome = request.getParameter("nome");
 		String apelido = request.getParameter("apelido");
 		String senha = request.getParameter("cpf");
-		TipoPerfilInvestidorEnum perfil = TipoPerfilInvestidorEnum.CONSERVADOR;
-		TipoFaturamentoEnum faturamento = TipoFaturamentoEnum.FIXO_MENSAL;
+		String senhaConfirma = request.getParameter("confirmarSenha");
+		TipoPerfilInvestidorEnum perfil = identificaPerfil(request);
+		TipoFaturamentoEnum faturamento = identificaFaturamento(request);
 		
-		Usuario user = new Usuario(cpf,nome,senha,apelido,perfil,faturamento);
-		System.out.println(user.toString());
-		try {
-			dao.insert(user);
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			System.out.println(e);
-			e.printStackTrace();
+		if (senha.equalsIgnoreCase(senhaConfirma)) {
+			
+			Usuario user = new Usuario(cpf, nome, senha, apelido, perfil, faturamento);
+			System.out.println(user.toString());
+			try {
+				UsuarioDAO dao = new UsuarioDAO();
+				dao.insert(user);
+				request.setAttribute("message", "Usuario cadastrado com sucesso");
+				request.getRequestDispatcher("login.jsp").forward(request, response);
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				System.out.println(e);
+				e.printStackTrace();
+				request.setAttribute("error", "erro ao inserir usuario no banco");
+				request.getRequestDispatcher("cadastro.jsp").forward(request, response);
+			}	
+		}else {
+			request.setAttribute("error", "Confirmacao de senha incorreta");
+			request.getRequestDispatcher("cadastro.jsp").forward(request, response);
 		}
 		System.out.println("==========POST=============");
-		doGet(request, response);
+	}
+
+	private TipoFaturamentoEnum identificaFaturamento(HttpServletRequest request) {
+		TipoFaturamentoEnum faturamento = null;
+		if (request.getParameter("fs") != null) {
+			faturamento = TipoFaturamentoEnum.FIXO_SEMANAL;
+		} else if (request.getParameter("fm") != null) {
+			faturamento = TipoFaturamentoEnum.FIXO_MENSAL;
+		} else if (request.getParameter("fa") != null) {
+			faturamento = TipoFaturamentoEnum.FIXO_ANUAL;
+		} else if (request.getParameter("vr") != null) {
+			faturamento = TipoFaturamentoEnum.VARIAVEL;
+		} else if (request.getParameter("mt") != null) {
+			faturamento = TipoFaturamentoEnum.MISTO;
+		} else {
+			faturamento = TipoFaturamentoEnum.NAO_INFORMADO;
+		}
+		return faturamento;
+	}
+
+	private TipoPerfilInvestidorEnum identificaPerfil(HttpServletRequest request) {
+		TipoPerfilInvestidorEnum perfil = null;
+
+		if (request.getParameter("experienciaSim") != null) {
+			if (request.getParameter("altoRiscoSim") != null) {
+				perfil = TipoPerfilInvestidorEnum.EXPERIENTE;
+			} else {
+				if (request.getParameter("altoRiscoNao") != null
+						|| (request.getParameter("assumeRisco") != null && request.getParameter("3-5") != null)) {
+					perfil = TipoPerfilInvestidorEnum.MODERADO;
+				}
+			}
+		} else {
+			perfil = TipoPerfilInvestidorEnum.CONSERVADOR;
+
+		}
+
+		return perfil;
 	}
 
 }
---
+++ src/main/webapp/login.jsp
@@ -0,0 +1,35 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+	pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<title>Insert title here</title>
+<link rel="stylesheet" href="./stylesheet/style-login.css">
+</head>
+<body>
+	<h2>Acesse sua conta</h2>
+	<img src="./img/login.png">
+	<form method="post" action="login">
+		<ul>
+			<li><label>CPF</label> <input type="text"
+				placeholder="xxx.xxx.xxx-xx" name="cpf" class="card"></li>
+			<li><label>Senha</label> <input type="text" placeholder="xxxxxx"
+				name="senha" class="card"></li>
+			<li class="invert"><a>Esqueceu sua senha?</a></li>
+			<li>
+				<div class="btn">
+					<input type="submit" value="Entrar">
+				</div>
+			</li>
+		</ul>
+
+
+	</form>
+	<div class="footer">
+		Não possui uma conta?<a href="./cadastro.jsp">Cadastre-se</a>
+	</div>
+
+
+</body>
+</html>
